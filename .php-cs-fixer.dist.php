44c9ecf5f9017bf45af4a5d0c9d91a65c4a868ae
Kei <budi03@gmail.com>
2024-07-11 02:40:06 +0700

    realodix/relax v1.15

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,25 +4,19 @@ use Realodix\Relax\Config;
 use Realodix\Relax\Finder;
 
 $localRules = [
-    'class_attributes_separation' => [
-        'elements' => ['const' => 'only_if_meta'],
-    ],
-    'native_function_invocation' => [
-        'include' => ['@internal'],
-    ],
-
-    // Realodix
+    // Relax
     'binary_operator_spaces' => [
         'operators' => [
             '=>' => 'align_single_space',
-            '='  => 'single_space', ],
+        ],
     ],
 ];
 
 $finder = Finder::base()
+    ->in(__DIR__)
     ->append(['.php-cs-fixer.dist.php']);
 
-return Config::create('Realodix')
+return Config::create('relax')
     ->setRules($localRules)
     ->setFinder($finder)
     ->setCacheFile(__DIR__.'/.tmp/.php-cs-fixer.cache');
